74212d6d5a704c9b1a8853da820093d8df0c2e4e
Ikki Kishimoto <k22rs047@st.kyusan-u.ac.jp>
2025-02-01 20:15:07 +0900

    10回　Hello, Java!　と出力

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 // Version: 1.0
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, Java!");
+        }
     }
 }
